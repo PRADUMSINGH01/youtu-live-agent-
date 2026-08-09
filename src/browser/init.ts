33956fce0677a69e4fafc1f9d2b3112232a255e6
PRADUMSINGH01 <hs9040301@gmail.com>
2026-08-09 15:15:14 +0530

    Fix: Enable SwiftShader CPU WebGL rendering for 3D physics canvas

--- src/browser/init.ts
+++ src/browser/init.ts
@@ -18,7 +18,7 @@ async function test() {
 	const browserOptions: any = isDocker ? {
 		// Production (Docker) settings
 		executablePath: "/usr/bin/chromium",
-		args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--start-maximized", "--autoplay-policy=no-user-gesture-required", "--window-size=1920,1080", "--no-first-run", "--no-default-browser-check", "--disable-gpu", "--disable-software-rasterizer"],
+		args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--start-maximized", "--autoplay-policy=no-user-gesture-required", "--window-size=1920,1080", "--no-first-run", "--no-default-browser-check", "--enable-webgl", "--use-gl=angle", "--use-angle=swiftshader"],
 		ignoreDefaultArgs: ["--mute-audio"],
 		defaultViewport: {
 			width: 1920,
